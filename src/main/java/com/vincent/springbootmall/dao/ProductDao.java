79fe256d042592a929527e1fed37e9ab4379156c
2311015 <killerplay13@gmail.com>
2024-01-10 15:26:15 +0800

    product - create product

--- src/main/java/com/vincent/springbootmall/dao/ProductDao.java
+++ src/main/java/com/vincent/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.vincent.springbootmall.dao;
 
+import com.vincent.springbootmall.dto.ProductRequest;
 import com.vincent.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
