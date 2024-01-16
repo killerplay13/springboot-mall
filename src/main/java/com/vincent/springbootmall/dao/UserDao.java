3e430ac7953d21e6f098d86b1c92f8056e413313
2311015 <killerplay13@gmail.com>
2024-01-16 14:21:16 +0800

    User - check email

--- src/main/java/com/vincent/springbootmall/dao/UserDao.java
+++ src/main/java/com/vincent/springbootmall/dao/UserDao.java
@@ -6,4 +6,6 @@ import com.vincent.springbootmall.model.User;
 public interface UserDao {
     Integer createUser(UserRegisterRequest userRegisterRequest);
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
 }
--- src/main/java/com/vincent/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/vincent/springbootmall/service/impl/UserServiceImpl.java
@@ -4,16 +4,27 @@ import com.vincent.springbootmall.dao.UserDao;
 import com.vincent.springbootmall.dto.UserRegisterRequest;
 import com.vincent.springbootmall.model.User;
 import com.vincent.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("email{} has been used",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return userDao.createUser(userRegisterRequest);
     }
 
